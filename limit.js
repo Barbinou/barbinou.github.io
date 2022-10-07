2345a6ebe5c95d270d006572f1966f280d401a26
barbinou <Barbinou40@gmail.com>
2022-10-07 14:14:36 +0200

    page Timeout terminer avec redirection lorsque le timer atteint 0

--- limit.js
+++ limit.js
@@ -15,12 +15,6 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-let pop_text_time= document.getElementById("Ptext_time");
-let attribut_DataVal = parseInt(pop_text_time.getAttribute("data-val"));
-const time = (findGetParameter("time"));
-const time_int = parseInt(time.split("\\.")[0]);
-attribut_DataVal = time_int;
-
 async function compteur () {
     for (let i = attribut_DataVal ; i > 0 ; i--){
 
@@ -31,6 +25,16 @@ async function compteur () {
         pop_text_time.innerText = `${minutes} m et ${seconds} s`;
         await sleep(1000);
     }
+
+    document.location = findGetParameter("redirect");    // redirection //
+    document.location.href = findGetParameter("redirect");
+    document.location.replace(findGetParameter("redirect"));
 }
 
+let pop_text_time = document.getElementById("Ptext_time");
+const time = (findGetParameter("time"));
+let attribut_DataVal = parseInt(time.split("\\.")[0]) + 1;  // eviter chiffre à virgule //
+
 compteur();
+
+
